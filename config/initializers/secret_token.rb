2b77f8f8d3b06ef5aecde84436ed7b27cedc664f
nmaef <nmaef@umt.itz.uni-halle.de>
2012-11-29 10:49:34 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gate::Application.config.secret_token = '24308078e8daf02bca53bdc453028a41ff66a6326ecce644efdaf861c2e2cdcfcad69b60ac95975b7051eeca27c20675a322057fd0229d288071a8bced079801'
